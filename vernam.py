59bc99da60bd499460ec6111313d5d7556fee954
MOUHIHA MOHAMED <59533527+mouhihaMohamed702@users.noreply.github.com>
2022-01-11 10:23:58 +0100

    Create vernam.py

---
+++ vernam.py
@@ -0,0 +1,20 @@
+def Vernam(Plain,Key,Flag):
+    result=""
+    for i in range(len(Plain)):
+        char=Plain[i]
+        if(Flag):
+            result+=chr((ord(char)-97 +ord(Key[i])-97)%26 +97)
+        else:
+            result += chr((ord(char) - ord(Key[i])+26) % 26 + 97)
+    return result
+# assumption:- for simplicity we are only considering lowercase-values and without spaces
+
+if __name__=="__main__":
+    Key=''.join(input("Enter Key: ").lower().split())
+    Plain=''.join(input("Enter Text: ").lower().split())
+    if(len(Key)!=len(Plain)):
+        print("Invalid Key!")
+        exit(None)
+    CipherText=Vernam(Plain,Key,True)
+    print("CipherText: ",CipherText)
+    print("PlainBack: ",Vernam(CipherText,Key,False))
